c63f9fb00b36f9ad1b583e3a3b2b5686c7cb8988
Eric Larson <eric@ionrock.org>
2015-01-15 13:25:50 -0600

    Can tail from dired locally and over ssh via tramp.

--- tailel.el
+++ tailel.el
@@ -16,17 +16,39 @@
 ;;; Commentary:
 ;; Some helpers and interactive functions to easily tail files and
 ;; command output.
+(require 's)
 
 
 (defun tailel-create-buffer-name (cmd)
   (format "*tail: %s*" cmd))
 
-(defun tailel-tail-command (cmd)
+(defun tailel-run-command (cmd)
   "Tail a file in its own buffer."
   (let ((buffer-namer  (lambda (mode)
 			 (tailel-create-buffer-name cmd))))
-    (compilation-start cmd nil buffer-namer)))
+    (compilation-start cmd t buffer-namer)))
+
+(defun tailel-run-tail (filename)
+  (tailel-run-command (tailel-tail filename)))
+
+(defun tailel-tail (path)
+  (format "tail -f %s" path))
 
 (defun tailel-tail-ssh-command (user host cmd)
   (let ((ssh-cmd (format "ssh %s@%s %s" user host cmd)))
-    (tailel-tail-command ssh-cmd)))
+    (tailel-run-command ssh-cmd)))
+
+(defun tailel-tail-ssh-dired (filename)
+  (save-match-data
+    (if (string-match "/scp:\\([^@]+\\)@\\([^@]+\\):\\(.*\\)" filename)
+	(let ((user (match-string 1 filename))
+	      (host (match-string 2 filename))
+	      (cmd (tailel-tail  (match-string 3 filename))))
+	(tailel-tail-ssh-command user host cmd)))))
+
+(defun tailel-tail-dired ()
+  (interactive)
+  (let ((filename (dired-get-filename)))
+    (if (s-starts-with? "/scp" filename)
+	(tailel-tail-ssh-dired filename)
+      (tailel-run-tail filename))))
